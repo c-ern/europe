6dbb920bd73c238ca2b4341f8ccb4511b217b045
Steve Lair <steve.lair@gmx.de>
2012-08-16 11:36:00 +0200

    Dateien nach rails generate europe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Europe::Application.config.secret_token = 'c0263ba7aa0e83a6760e453585993a512df049a8bd50a5f57afff329838d40aafd7538353e8e50556eb8edb5499913188166016eb88335ea75bc077df2ab475c'
